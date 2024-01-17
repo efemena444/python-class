d8c4dcf1372728087481fb3822aff56560a9385e
Efemena444 <efemenagift59@gmail.com>
2024-01-17 04:44:12 -0500

    my first python file

---
+++ sum.py
@@ -0,0 +1,5 @@
+number1 =int( input("the value of the fisrt digit:"))
+number2 =int( input("the value of the second digit:"))
+number3 =int( input("the value of the third digit:"))
+sum = number1 + number2 + number3
+print("the sum of three digits ",sum)
