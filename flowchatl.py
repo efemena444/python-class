d1b8ef3ee82d690d198a53289ae98734b1459c1c
Efemena444 <efemenagift59@gmail.com>
2024-01-19 05:43:11 -0500

    flowchatl.py

---
+++ flowchatl.py
@@ -0,0 +1,13 @@
+stuBmi = (int(input("enter value of your bm")))
+if stuBmi < 16:
+   print('you cant ride')
+else:
+    print("you can ride")
+    distance = int(input("enter the24 values between 21 -24 "))  
+    if stuBmi == 21:
+        print("you can take a ride to 28m")
+        if stuBmi == 22:
+            print("you can take a ride to 30m")
+            if stuBmi == 24:
+                print("you can take a ride to 80m")
+                      
