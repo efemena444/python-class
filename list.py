b4c082e8bd183e754de51c1c361321da66b0678d
Efemena444 <efemenagift59@gmail.com>
2024-02-02 12:52:51 -0500

    list.py

--- list.py
+++ list.py
@@ -2,13 +2,12 @@ import random
 secret_number = random.randint(1,100)
 print("welcome to guessing game")
 for guess_count in range(0,5):
-   guess = (input("guess the random number:"))
+   guess =int(input("guess the random number:"))
    
    if guess == secret_number:
     print("congratulations")
-    print("secret_number")    
     break
-   elif guess< secret_number:
+   elif guess  < secret_number:
        print("too low")
    elif guess > secret_number:
        print("TOO HIGH")
